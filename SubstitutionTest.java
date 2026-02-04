c82d65045f8cadfccf0437f73b568de71d1f6d56
akashpenke-cmyk <akash.penke@sasi.ac.in>
2026-02-04 09:38:45 +0530

    Update SubstitutionTest.java

--- SubstitutionTest.java
+++ SubstitutionTest.java
@@ -1,7 +1,14 @@
+/*
+Name  : Penke Akash
+Class : CSE A (3rd Year)
+RegNo : 24K65A0512
+*/
+
 import java.util.*;
 
 class SubstitutionTest {
     public static void main(String args[]) {
+
         Scanner ak = new Scanner(System.in);
         String text, ctext = "", dtext = "";
         String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -12,12 +19,12 @@ class SubstitutionTest {
 
         for (int i = 0; i < text.length(); i++) {
             char a = text.charAt(i);
+
             for (int j = 0; j < 26; j++) {
-                if (a == alphabet.charAt(j)) {
+                if (a == alphabet.charAt(j))
                     ctext += key.charAt(j);
-                } else if (a == ' ') {
-                    ctext = ctext + " ";
-                }
+                else if (a == ' ')
+                    ctext += " ";
             }
         }
 
@@ -25,12 +32,12 @@ class SubstitutionTest {
 
         for (int i = 0; i < ctext.length(); i++) {
             char a = ctext.charAt(i);
+
             for (int j = 0; j < 26; j++) {
-                if (a == key.charAt(j)) {
+                if (a == key.charAt(j))
                     dtext += alphabet.charAt(j);
-                } else if (a == ' ') {
-                    dtext = dtext + " ";
-                }
+                else if (a == ' ')
+                    dtext += " ";
             }
         }
 
