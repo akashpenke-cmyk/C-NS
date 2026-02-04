09dda4d1961d6830688476d93960ea495aac7101
akashpenke-cmyk <akash.penke@sasi.ac.in>
2026-02-04 09:37:17 +0530

    Update CipherText.java

--- CipherText.java
+++ CipherText.java
@@ -1,34 +1,50 @@
+/*
+Name  : Penke Akash
+Class : CSE A (3rd Year)
+RegNo : 24K65A0512
+*/
+
 import java.util.*;
+
 class CipherText {
     public static void main(String args[]) {
+
         String text, ctext = "", dtext = "";
         int shift, i;
         Scanner ak = new Scanner(System.in);
+
         System.out.println("Enter a text:");
         text = ak.nextLine();
+
         System.out.println("Enter Shift Value:");
         shift = ak.nextInt();
+
+        // Encryption
         for (i = 0; i < text.length(); i++) {
             char t = text.charAt(i);
-            if (t >= 'A' && t <= 'Z') {
-                ctext = ctext + (char) ((t - 'A' + shift) % 26 + 'A');
-            } else if (t >= 'a' && t <= 'z') {
-                ctext = ctext + (char) ((t - 'a' + shift) % 26 + 'a');
-            } else {
-                ctext = ctext + t;
-            }
+
+            if (t >= 'A' && t <= 'Z')
+                ctext += (char) ((t - 'A' + shift) % 26 + 'A');
+            else if (t >= 'a' && t <= 'z')
+                ctext += (char) ((t - 'a' + shift) % 26 + 'a');
+            else
+                ctext += t;
         }
+
         System.out.println("THE CIPHER TEXT IS: " + ctext);
-        for (i = 0; i < text.length(); i++) {
+
+        // Decryption
+        for (i = 0; i < ctext.length(); i++) {
             char t = ctext.charAt(i);
-            if (t >= 'A' && t <= 'Z') {
-                dtext = dtext + (char) ((t - 'A' + 26 - shift) % 26 + 'A');
-            } else if (t >= 'a' && t <= 'z') {
-                dtext = dtext + (char) ((t - 'a' + 26 - shift) % 26 + 'a');
-            } else {
-                dtext = dtext + t;
-            }
+
+            if (t >= 'A' && t <= 'Z')
+                dtext += (char) ((t - 'A' + 26 - shift) % 26 + 'A');
+            else if (t >= 'a' && t <= 'z')
+                dtext += (char) ((t - 'a' + 26 - shift) % 26 + 'a');
+            else
+                dtext += t;
         }
+
         System.out.println("THE PLAIN TEXT IS: " + dtext);
     }
 }
